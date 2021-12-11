dbcdf31436119ae24784af67b32422531d868448
Nagesh T <nagt@publicisgroupe.net>
2021-12-11 14:01:05 +0530

    change button name

--- scripts.js
+++ scripts.js
@@ -4,14 +4,14 @@ let close_btn = document.querySelector('.close-btn');
 
 open_btn.addEventListener('click', () => {
     cookies_box.classList.toggle('active');
-    if(open_btn.innerText === "Show PopUp"){
-        open_btn.innerText = "Hide PopUp"
+    if(open_btn.innerText === "Show Cookies & Privacy"){
+        open_btn.innerText = "Hide Cookies & Privacy"
     }else{
-        open_btn.innerText = "Show PopUp" 
+        open_btn.innerText = "Show Cookies & Privacy" 
     }
 })
 
 close_btn.addEventListener('click', () => {
     cookies_box.classList.remove('active');
-    open_btn.innerText = "Show PopUp";
+    open_btn.innerText = "Show Cookies & Privacy";
 })
